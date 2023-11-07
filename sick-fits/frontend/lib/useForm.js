a2c0cbcc2b204743a9b47d2cc8dcf693ab71ef3b
Kristen Nakamura <kristen.nakamura@gmail.com>
2023-11-07 11:00:05 -0500

    form util for handling form elements

---
+++ sick-fits/frontend/lib/useForm.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // create a state object for our fields
+  const [inputs, setInputs] = useState(initial);
+
+  function handleChange(e) {
+    let { name, type, value } = e.target;
+
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+
+    if (type === 'file') {
+      value = e.target.file;
+    }
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
+
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
