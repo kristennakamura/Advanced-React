a90127e7ba20693145e75779884f11a9d0c4e03f
Kristen Nakamura <kristen.nakamura@gmail.com>
2023-11-14 11:42:17 -0500

    added custom type apollo cache for pagination

---
+++ sick-fits/frontend/lib/paginationField.js
@@ -0,0 +1,44 @@
+import { PAGINATION_QUERY } from '../components/Pagination';
+
+export default function paginationField() {
+  return {
+    keyArgs: false,
+    read(existing = [], { args, cache }) {
+      // asks the read fxn for those items
+      const { skip, first } = args;
+
+      // read number of items on page from cache
+      const data = cache.readQuery({ query: PAGINATION_QUERY });
+      const count = data?._allProductsMeta?.count;
+      const page = skip / first + 1;
+      const pages = Math.ceil(count / first);
+
+      const items = existing.slice(skip, skip + first).filter((x) => x);
+      if (items.length && items.length !== first && page === pages) {
+        return items;
+      }
+
+      if (items.length !== first) {
+        return false;
+      }
+
+      if (items.length) {
+        return items;
+      }
+
+      return false;
+    },
+    merge(existing, incoming, { args }) {
+      const { skip } = args;
+      // this runs when the Apollo client comes back with our products
+      // how do you want to put them in the cache?
+      console.log(`Merging items from the network ${incoming.length}`);
+      const merged = existing ? existing.slice(0) : [];
+      for (let i = skip; i < skip + incoming.length; ++i) {
+        merged[i] = incoming[i - skip];
+      }
+
+      return merged;
+    },
+  };
+}
